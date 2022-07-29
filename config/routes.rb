e383fed1de79a812386924c45600ee17a5899925
Duong Nguyen <duongnguyen@Duongs-MacBook-Pro.local>
2022-07-29 09:07:34 +0700

    Make user controller, post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registration: 'users/registrations'
+  }
+
   resources :posts
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# User.create(email: "duong.nguyen@eastagile.com", password: "730110Duong", password_confirmation: "password")
+User.create(email: "joebiden@us.gov", password: "123456789", password_confirmation: "123456789")
+
+# Post.create(title: "Fee is expected to increase 10% in next year", body: "Due to the inflation rate recently, we must
+# increase the fee by 10% in order to assure our teaching quality", user_id: User.first.id)
+2.times do |x|
+  Post.create(title: "Ruby on Rails is the best framework for web development #{x}", body: "There are a lot of developers are
+moving to Rails because of their simplicity", user_id: User.second.id)
+end
