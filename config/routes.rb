e2bbc8b9e6387710974370ac47023373c5704429
Duong Nguyen <duongnguyen@Duongs-MacBook-Pro.local>
2022-08-02 10:07:02 +0700

    Finished the rich text comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/profile'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registration: 'users/registrations'
+    registrations: 'users/registrations'
   }
-
-  resources :posts
+  get '/u/:id', to: 'users#profile', as: 'user'
+  # /posts/1/comments/4
+  resources :posts do
+    resources :comments
+  end
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
