3b2eea2bcc278fd09adb2088223b95099a9c06fc
Duong Nguyen <duongnguyen@Duongs-MacBook-Pro.local>
2022-08-03 15:45:53 +0700

    Finish the chart

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin_all_from "app/javascript/chart", under: "chart"
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@3.8.2/dist/chart.esm.js"
